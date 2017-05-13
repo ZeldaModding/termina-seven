6b145fba9b53af4e183bc7eb22a589866ef69553
Connor Olding <cloningdonor@gmail.com>
2017-05-13 08:54:02 +0000

    use airbnb linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,15 +8,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: 'airbnb-base',
   plugins: [
     'html'
   ],
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
